758bb4cab278ddf200d9c44a7f4c558f9aa396bd
Diverse41 <morenolms41@gmail.com>
2025-03-31 20:30:23 +0200

    Modifiche alla configurazione vite per poter usare il comando --host per testare sui vari dispositivi in locale

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/occhialy/" : "/",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true
+  }
 });
