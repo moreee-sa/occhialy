fdf14b9e6afb8b9aa1142bd945f4792e4f531532
Diverse41 <morenolms41@gmail.com>
2025-04-01 14:41:57 +0200

    Rimosso componente "in costruzione" e creata la base della pagina Collection

--- src/Collection.jsx
+++ src/Collection.jsx
@@ -1,5 +1,16 @@
 import Navbar from "./components/Navbar"
 import styled from "styled-components"
+import '@fontsource/libre-baskerville';
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+import "@fontsource/noto-sans/600.css";
+import LoremIpsum from "react-lorem-ipsum";
+
+const CollectionWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  box-sizing: border-box;
+`;
 
 const Content = styled.div`
   display: flex;
@@ -12,31 +23,76 @@ const Content = styled.div`
   /* background-color: #b18282; */
 `;
 
-const ConstructionMessage = styled.div`
+const TextWrapper = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  color: #ff6347;
-  font-weight: bold;
+  flex-direction: column;
+  color: #000000;
   text-align: center;
-  background-color: #f0f0f0;
+`;
 
+const TaglineWrapper = styled.div`
+  width: 50%;
+  /* background-color: red; */
+`;
+
+const Tagline = styled.p`
   font-family: "Libre Baskerville", sans-serif;
   font-weight: 400;
-  font-size: clamp(24px, 5vw, 42px);
-  line-height: 63px;
-  color: rgb(114, 110, 110);
+  
+  @media only screen and (min-width: 1200px) {
+    font-size: 86px;
+    line-height: 112px;
+  }
+
+  @media only screen and (max-width: 1200px) {
+    font-size: 38px;
+    line-height: 50px;
+  }
+`;
+
+const SubTaglineWrapper = styled.div`
+  width: 80%;
+  /* background-color: red; */
+`;
+
+const SubTagline = styled.p`
+  font-family: "Libre Baskerville", sans-serif;
+  font-weight: 400;
+  margin-top: 20px;
+  font-size: 22px;
+  line-height: 40px;
+`; 
+
+const EditionParagraph = styled.p`
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 30px;
+  color: #898989;
 `;
 
 function Collection() {
   return (
-    <Content>
-      <Navbar fade={false} inverted={true} />
-      <ConstructionMessage>
-          Ancora in costruzione...
-      </ConstructionMessage>
-    </Content>
+    <CollectionWrapper>
+      <Content>
+        <Navbar fade={false} inverted={true} />
+        <TextWrapper>
+          <TaglineWrapper>
+            <Tagline>Collezione 2025</Tagline>
+          </TaglineWrapper>
+          <EditionParagraph>Edizione Numerata</EditionParagraph>
+          <SubTaglineWrapper>
+            <SubTagline>
+              <LoremIpsum />
+            </SubTagline>
+          </SubTaglineWrapper>
+        </TextWrapper>
+      </Content>
+    </CollectionWrapper>
   )
 }
 
