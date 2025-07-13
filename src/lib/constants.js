73203439cb491b92ba82d4bcd8f8007054c3ece8
moreee-sa <morenolms41@gmail.com>
2025-07-13 23:43:45 +0200

    Riorganizzati i colori in un oggetto COLORS per migliorare la struttura e la scalabilità

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,9 +4,14 @@ export const MOBILEBREAKPOINT = "767px";
 export const TABLETBREAKPOINT = "960px";
 
 // Colors
-export const LIGHT_PRIMARY_COLOR = "#ffffffff";
 export const DARK_PRIMARY_COLOR = "#000000";
 export const SECONDARY_TEXT_COLOR = "#1d1d1f";
+
+export const COLORS = {
+  LIGHT_PRIMARY: "#ffffffff",
+  DARK_PRIMARY: "#000000",
+  SECONDARY_TEXT: "#1d1d1f",
+};
 
 // Fonts
 export const FONTS = {
