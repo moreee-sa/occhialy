7ccf3fe1e891df99646683b3b530f903ab052c46
moreee-sa <morenolms41@gmail.com>
2025-07-13 23:25:08 +0200

    Aggiunti nuovi colori al file delle costanti

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,9 @@
+// Breakpoints
 export const SMALLMOBILEBREAKPOINT = "480px";
 export const MOBILEBREAKPOINT = "767px";
 export const TABLETBREAKPOINT = "960px";
+
+// Colors
+export const LIGHT_PRIMARY_COLOR = "#ffffffff";
+export const DARK_PRIMARY_COLOR = "#000000";
+export const SECONDARY_TEXT_COLOR = "#1d1d1f";
