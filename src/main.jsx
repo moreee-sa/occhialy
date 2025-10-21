43c531e92e47187f98f939c91024f4d9703c243c
moreee-sa <76905821+moreee-sa@users.noreply.github.com>
2025-10-21 16:10:51 +0200

    Scroll to top on route change (#11)
    * Creato componente “Scroll to top” che esegue lo scroll in alto ogni volta che cambia la route
    
    * Aggiunto componente “Scroll to top”

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop.jsx'
 import './index.css'
 import App from './App.jsx'
 import About from './About.jsx'
@@ -10,6 +11,7 @@ import Contacts from './Contacts.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
